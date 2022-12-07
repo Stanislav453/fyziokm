383c0a2cd72451e2274b214227250f51cb61f71e
Stanislav Jendrišák <st.jendrisak@gmail.com>
2022-12-07 09:31:52 +0100

    Naimportoval som myHeader

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.scss';
+import myHeader from './components/myHeader/myHeader';
 
 function App() {
   return (
     <section  >
-      <h1>hellov world</h1>
+      <myHeader />
     </section>
   );
 }
