4314f3481ed63ff5741b50939ea406a5007765f1
Stanislav Jendrišák <st.jendrisak@gmail.com>
2022-12-07 09:30:55 +0100

    Vytvoril som componenty a spravil im základnú štruktúru.

---
+++ src/components/myHeader/myHeader.js
@@ -0,0 +1,9 @@
+import "./myHeader.scss";
+
+const myHeader = () => {
+    return (
+        <h2>MyHeader</h2>
+    );
+};
+
+export default myHeader
---
+++ src/components/myMenu/myMenu.js
@@ -0,0 +1,9 @@
+import "./myMenu.scss";
+
+const myMenu = () => {
+    return (
+        <h2>MyMenu</h2>
+    );
+};
+
+export default myMenu
