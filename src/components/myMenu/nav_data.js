b584d9f6fb2f460f0fbaea4d9ce8e3c019df2345
Stanislav Jendrišák <st.jendrisak@gmail.com>
2022-12-08 09:12:30 +0100

    Vytvoril som data pre navigaciu.

---
+++ src/components/myMenu/nav_data.js
@@ -0,0 +1,26 @@
+export const data = [
+    {
+        name: "O mne",
+        link: "#omne"
+    },
+    {
+        name: "Cenník",
+        link: "#cennik"
+    },
+    {
+        name: "Služby",
+        link: "sluzby"
+    },
+    {
+        name: "Metódy",
+        link: "#metody"
+    },
+    {
+        name: "Kontakt",
+        link: "#kontakt"
+    },
+    {
+        name: "Fotogaleria",
+        link: "http://fyziokm.sk/fotogaleria.html"
+    },
+]
