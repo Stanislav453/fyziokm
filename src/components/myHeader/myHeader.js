d29e07665b3ec8b2295dc0d976f4d73036e113db
Stanislav Jendrišák <st.jendrisak@gmail.com>
2022-12-10 17:14:32 +0100

    pridla som .baner-container .banner

--- src/components/myHeader/myHeader.js
+++ src/components/myHeader/myHeader.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./myHeader.scss";
-import MyMenu from "../myMenu/MyMenu";
+import MyMenu from "../MyMenu/MyMenu";
 
 const MyHeader = () =>  {
     return (
         <div className="my-header">
             <MyMenu />
+            <div className="baner-container">
+                <div className="banner">
+                    <h3>Zariadenie funguje na priamu platbu, nie na poisťovňu.</h3>
+                </div>
+            </div>
         </div>
     )
 }
