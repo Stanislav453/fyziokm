314f6aacfeefc989cd23b94ac92bf48f0d5c7332
Stanislav Jendrišák <st.jendrisak@gmail.com>
2022-12-11 12:56:36 +0100

    vytvoril som funkciu myMenu a pridal .container-nav .mobile-menu-icon

--- src/components/myMenu/myMenu.js
+++ src/components/myMenu/myMenu.js
@@ -1,22 +1,34 @@
-import React from "react";
+import React,{ useState }from "react";
 import { data } from "./nav_data";
-import { FaInstagram, FaFacebook  } from "react-icons/fa"
-import logo from "./logo.svg"
+import { FaInstagram, FaFacebook, FaBars, FaBan  } from "react-icons/fa"
+import logo from "../../logo.svg"
 import "./MyMenu.scss";
 
-const myMenu = () => {
+const MyMenu = () => {
+
+    const [ clicked, setClicked ] = useState(true)
+
+    const  onlyClick = () => {
+       setClicked( !clicked )
+
+    }
+
     return (
+    <div className="container-nav">
         <nav className="navigation">
             <div className="icons">
                 <a href="https://www.instagram.com/michaleckristian/"><FaInstagram /></a>
                 <a href="https://www.facebook.com/kristian.michalec"><FaFacebook /></a>
             </div>
-            <ul className="link-list">
+            <div className="mobile-menu-icon">
+               <i className="hamburger" onClick={ () => { onlyClick() } } > { clicked ? <FaBars /> :  <FaBan /> } </i> 
+            </div>
+            <ul className={clicked ? "link-list" : "link-list active"}>
             {
                 data.map( (item,key) => {
                     return(
-                       <li key={key}>
-                            <a href={item.link}>{item.name}</a>
+                       <li  key={key}>
+                            <a onClick={ () => { setClicked(true) } }  href={item.link}>{item.name}</a>
                        </li>
                     )
                 })
@@ -24,7 +36,8 @@ const myMenu = () => {
             <a href="#"><img src={logo} alt="logo" width="30" height="30" className="logo" /></a>
             </ul>
         </nav>
+    </div>
     );
 };
 
-export default myMenu
+export default MyMenu
