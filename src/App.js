2f3467501f6808a0a8b9c1f1aece05f4fee6d2c4
Stanislav Jendrišák <st.jendrisak@gmail.com>
2022-12-11 18:32:53 +0100

    pridla som main .main-container <AboutMe />

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.scss';
 import MyHeader from './components/myHeader/myHeader';
+import AboutMe from './components/AboutMe/AboutMe';
 
 function App() {
   return (
     <section  >
-      <MyHeader />
+      <header>
+        <MyHeader />
+      </header>
+      <main>
+        <div className='main-container'>
+          <AboutMe />
+        </div>
+      </main>
     </section>
   );
 }
