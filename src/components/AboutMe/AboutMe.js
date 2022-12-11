6d7acd33f69296125ed9995078de4fa54d5210f7
Stanislav Jendrišák <st.jendrisak@gmail.com>
2022-12-11 17:13:45 +0100

    Vytvoril som základnú štruktúru súborov.

---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,14 @@
+import "./AboutMe.scss";
+
+
+const AboutMe = () => {
+    return (
+        <div className="about-me">
+            <header>
+                <h2>O mne</h2>
+            </header>
+        </div>
+    )
+}
+
+export default AboutMe
