ad885639e2d1852dd800b190804c6a13c04f65f4
Stanislav Jendrišák <st.jendrisak@gmail.com>
2022-12-11 12:54:37 +0100

    pridal som .info-baner .header-logo .name-contact .contact-container .my-icon

--- src/components/myHeader/myHeader.js
+++ src/components/myHeader/myHeader.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./myHeader.scss";
 import MyMenu from "../MyMenu/MyMenu";
+import logo from "../../logo.svg"
+import { FaPhoneAlt,FaHome, FaEnvelope,FaMapMarkerAlt   } from "react-icons/fa";
 
 const MyHeader = () =>  {
     return (
@@ -9,6 +11,20 @@ const MyHeader = () =>  {
             <div className="baner-container">
                 <div className="banner">
                     <h3>Zariadenie funguje na priamu platbu, nie na poisťovňu.</h3>
+                </div>
+                <div className="info-baner">
+                    <img src={logo} width="250" height="250" alt="logo" className="header-logo" />
+                    <h4>NEŠTÁTNE ZDRAVOTNÍCKE ZARIADENIE</h4>
+                    <div className="name-contact">
+                        <h1>Mgr. Kristián Michalec</h1>
+                        <div className="contact-container">
+                            <ul>
+                                <li><FaMapMarkerAlt className="my-icon" />023 02 Krásno nad Kysucou, Kalinovská 1202 (rodinný dom)</li>
+                                <li><a href="mailto:kristian.michalec1111@gmail.com"><FaEnvelope className="my-icon"/>kristian.michalec1111@gmail.com</a></li>
+                                <li><a href="tel:+421918239799"><FaPhoneAlt className="my-icon" />+421918239799</a></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
