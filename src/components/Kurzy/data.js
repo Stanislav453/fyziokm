8b1579d670d664d0715919dcf746d31b6efe29d4
Stanislav Jendrišák <st.jendrisak@gmail.com>
2022-12-18 18:14:46 +0100

    Pridal som nové data.

---
+++ src/components/Kurzy/data.js
@@ -0,0 +1,148 @@
+const data = [ {
+    year: "2022",
+    list : [
+        { title: "Mulligan",
+          desc: "- dolný kvadrant (Praha -Fyziokurzy.cz) " 
+        },
+        { title: "Neuro-muskulo-skeletálny koncept C",
+          desc: "(Tatranská Polianka-Physioplus)" 
+        },
+        { title: "Fasciálna manipulácia",
+          desc: "- Stecco (FM 2) (Veľký Slavkov - Syntonic.pl)" 
+        },
+        { title: "Fasciálna manipulácia",
+          desc: "- Stecco (FM 1) (Veľký Slavkov - Syntonic.pl)" 
+        },
+        { title: "Dynamická neuromuskulárna stabilizácia B",
+        desc: "(Bratislava - Rehabilitation Prague School)" 
+        },
+        { title: "SI diagnostika a rehabilitácia",
+          desc: "(Online - Svět fyzioterapie)" 
+        },
+        { title: "Terapia a diagnostika bedra a panvového pletenca",
+          desc: "(Bratislava - Fyziokurzy.cz)" 
+        },
+
+    ]
+},
+{
+    year: "2021",
+    list: [
+        { title: "Neuro-muskulo-skeletálny koncept B",
+          desc: "(Tatranská Polianka- Physioplus) " 
+        },
+        { title: "Terapeutické využitie flossbandu",
+          desc: "(Bratislava- Rehasport.sk)" 
+        },
+        { title: "Mobilizácie",
+          desc: "(Bratislava- Svět fyzioterapie)" 
+        },
+        { title: "Mojžíšova A- mobilizácie rebier",
+          desc: "(Žilina- Rehabko)" 
+        },
+        { title: "Nordic Walking - základný kurz",
+        desc: "(Štrbské Pleso- Slovenská Nordic Walking asociácia)" 
+        },
+        { title: "Mulligan - horný kvadrant",
+          desc: "(Bratislava- Zdravo a fit)" 
+        },
+        { title: "Dynamická neuromuskulárna stabilizácia A",
+          desc: "(Bratislava- Rehabilitation Prague School)" 
+        },
+        { title: "Diastáza",
+        desc: "(Online- Miracle štúdio)" 
+        },
+        { title: "Tehotenstvo a fitlopta",
+          desc: "(Online- Miracle štúdio)" 
+        },
+        { title: "Tehotenstvo a pohyb",
+          desc: "(Online- Miracle štúdio)" 
+        },
+    ]
+
+},
+{
+    year: "2020",
+    list: [
+        { title: "Neuro-muskulo-skeletálny koncept A",
+          desc: "(Tatranská Polianka- Physioplus)" 
+        },
+        { title: "Terapia a diagnostika členkového kĺbu",
+          desc: "(Bratislava- Fyziokurzy.cz)" 
+        },
+        { title: "Chôdza a beh",
+          desc: "(Ostrava- Fyzio Beskyd)" 
+        },
+        { title: "SM- systém CD",
+          desc: "(Trenčín- RehabMedica)" 
+        },
+    ]
+
+},
+{
+    year: "2019",
+    list: [
+        { title: "Terapia a diagnostika kolenného kĺbu",
+          desc: "(Bratislava- Fyziokurzy.cz)" 
+        },
+        { title: "Managing Sports Injuries",
+          desc: "(Trenčín- Sport Therapy)" 
+        },
+        { title: "Terapia a diagnostika ramenného kĺbu",
+          desc: "(Bratislava- Fyziokurzy.cz)" 
+        },
+        { title: "Terapeutické využitie Thera-guna",
+          desc: "(Piešťany- Rehasport.sk)" 
+        },
+        { title: "Dornova metóda plus I.",
+        desc: "(Bratislava- Dornova metóda plus)" 
+        },
+        { title: "Terapeutické využitie Thera-guna",
+        desc: "(Piešťany- Rehasport.sk)" 
+        },
+    ]
+
+},
+{
+    year: "2018",
+    list: [
+        { title: "Fyzioterapia a fyziotréning panvového dna",
+          desc: "(Bratislava- Zdravo a fit)" 
+        },
+        { title: "Klapovo lezenie v rámci kompenzačného tréningu aktívnych športovcov",
+          desc: "(Bratislava- Zdravo a fit)" 
+        },
+        { title: "Manuálna lymfodrenáž",
+          desc: "(Bratislava- Zdravo a fit)" 
+        },
+        { title: "SM- systém AB",
+          desc: "(Trenčín- RehabMedica)" 
+        },
+    ]
+
+},
+{
+    year: "2017",
+    list: [
+        { title: "Bankovanie",
+          desc: "(Bratislava- AIMI)" 
+        },
+        { title: "Klasická masáž",
+          desc: "(Bratislava- Zdravo a fit)" 
+        },
+        { title: "Reflexná masáž",
+          desc: "- segmentová (Piešťany- AIMI)" 
+        },
+        { title: "Terapeutické využitie Kinesio tapu",
+          desc: "(Piešťany- Rehasport) " 
+        },
+        { title: "Reflexná masáž chodidla",
+        desc: "(Piešťany- AIMI) " 
+        },
+    ]
+
+},
+
+]
+
+export default data
