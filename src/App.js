98dc8d50a6ecf5672ea24d556d3ed156deaee8c8
Stanislav Jendrišák <st.jendrisak@gmail.com>
2022-12-27 20:00:57 +0100

    importol som Sluzby, Method, Kontakt, Footer, ScrollTop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import MyHeader from './components/myHeader/myHeader';
 import AboutMe from './components/AboutMe/AboutMe';
 import Cennik from './components/Cennik/Cennik';
 import Kurzy from './components/Kurzy/Kurzy';
+import Sluzby from './components/Sluzby/Sluzby';
+import Method from './components/Method/Method';
+import Kontakt from './components/Kontakt/Kontakt';
+import Footer from './components/Footer/Footer';
+import ScrollTop from './components/ScrollTop/ScrollTop';
 
 function App() {
+
   return (
     <section  >
       <header>
@@ -16,8 +22,15 @@ function App() {
           <AboutMe />
           <Cennik />
           <Kurzy />
+          <Sluzby />
+          <Method />
+          <Kontakt />
+          <ScrollTop />
         </div>
       </main>
+      <footer>
+        <Footer />
+      </footer>
     </section>
   );
 }
