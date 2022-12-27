ae4138c19e156f730149885b0da4ab82897e3813
Stanislav Jendrišák <st.jendrisak@gmail.com>
2022-12-27 20:09:04 +0100

    Pridal som data

---
+++ src/components/Sluzby/data.js
@@ -0,0 +1,55 @@
+ const data = [
+    {
+        title: "Individuálna fyzioterapia",
+        list: [
+            {
+            under_title: "Fyzioterapia zameraná na bolestivé stavy pohybového ",
+            desc:" bolesti hlavy, bolesti krčnej chrbtice, bolesti driekovej chrbtice, migrény, bolesti v oblasti kĺbov horných a dolných končatín, blokáda rebier... "
+            },
+            {    
+            under_title: "Fyzioterapia zameraná na pooperačné stavy ",
+            desc:" po artroskopii kĺbov, po výmene bedier/ramien/kolien, po zošrobovaní stavcov/kosti, po plastikách väzoch, po operáciach chrbtice,... "
+            },
+            {    
+            under_title: "Fyzioterapia u detských pacientov ",
+            desc:"chybné držanie tela, funkčné skoliózy, ploché nohy,... "
+            },
+            {
+            under_title: "Fyzioterapia pri poraneniach mäkkých tkanív ",
+            desc:"tenisový/golfový lakeť, zápaly šliach, burzitídy, natrhnutie/natiahnutie väzov, problémy z achilovou šľachou,.. "
+            },
+            {    
+            under_title: "Fyzioterapia pre ženy",
+            desc:"problémy s pohybovým aparátom v tehotenstve a po pôrode, inkontinencia, osteoporóza,... "
+            },
+            {    
+            under_title: "Fyzioterapia u jednoduchých neurologických diagnóz",
+            desc:"poškodenie v oblasti chrbtice, karpálny tunel, CC-syn., CB-syn., LIS,... "
+            },
+            {    
+            under_title: "Fyzioterapia u reumatických ochorení",
+            desc:"reumatoidná artritída, Bechterev,... "
+            },
+            {    
+            under_title: "Fyzioterapia zameraná na funkčné problémy pohybového aparátu",
+            },
+        ]
+    },
+    {
+        title: "Športová fyzioterapia",
+        list: [
+            {    
+            under_title: "Fyzioterapia u športovcoch po úrazoch ",
+            desc:"rôzne tipy poškodenia členkového kĺbu výron/distorzia, natiahnutie/natrhnutie svalov, stavy po zlomeninách a po vykĺbení kĺbov "
+            },
+            {    
+            under_title: "Preventívna fyzioterapia",
+            desc:"zameraná na kompenzačné cvičenie, nastavenie vhodnej regenerácie po záťaži, edukácia športovcov,... "
+            },
+        ]
+    }
+
+]
+
+export default data
+
